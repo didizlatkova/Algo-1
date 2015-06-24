718f337ae0f6b90207ade7214595b980b78171f2
Didi <didi.zlatkova93@gmail.com>
2015-06-24 20:26:37 +0300

    added method remove to bst

--- src/week3/trees/phonebook/PhoneBook.java
+++ src/week3/trees/phonebook/PhoneBook.java
@@ -2,16 +2,16 @@ package week3.trees.phonebook;
 
 public class PhoneBook {
 
-	private ContactNode node;
+	private ContactNode firstNode;
 
 	// inserts a new contact
 	public void insert(Contact contact) {
-		if (node == null) {
-			node = new ContactNode(contact);
+		if (firstNode == null) {
+			firstNode = new ContactNode(contact);
 			return;
 		}
 
-		ContactNode nextNode = node;
+		ContactNode nextNode = firstNode;
 		while (true) {
 			if (contact.name.compareTo(nextNode.value.name) < 0) {
 				if (nextNode.left != null) {
@@ -36,47 +36,102 @@ public class PhoneBook {
 
 	// lookup a name and print its phone number
 	public void lookup(String name) {
-		ContactNode nextNode = node;
+		ContactNode found = lookupNode(name);
+		if (found != null) {
+			System.out.println(found.value.number);
+		} else {
+			System.out.println("NOT FOUND!");
+		}
+	}
+
+	private ContactNode lookupNode(String name) {
+		ContactNode nextNode = firstNode;
 		while (true) {
 			if (name.compareTo(nextNode.value.name) < 0) {
 				if (nextNode.left != null) {
 					nextNode = nextNode.left;
 				} else {
-					System.out.println("NOT FOUND!");
-					break;
+					return null;
 				}
 			} else if (name.compareTo(nextNode.value.name) > 0) {
 				if (nextNode.right != null) {
 					nextNode = nextNode.right;
 				} else {
-					System.out.println("NOT FOUND!");
-					break;
+					return null;
 				}
 			} else {
-				System.out.println(nextNode.value.number);
-				break;
+				return nextNode;
 			}
 		}
 	}
 
 	// list all records in an alphabetical order
 	public void list() {
-		traverse(node);
+		traverse(firstNode);
 	}
 
 	private void traverse(ContactNode node) {
 		if (node == null) {
 			return;
 		}
-		
+
 		traverse(node.left);
 		System.out.println(node.value.name + " " + node.value.number);
-		traverse(node.right);		
+		traverse(node.right);
 	}
 
 	// remove a record for a given name
 	public void remove(String name) {
+		ContactNode nodeToRemove = lookupNode(name);
+		if (nodeToRemove != null) {
+			remove(nodeToRemove);
+		}
+	}
 
+	private void remove(ContactNode node) {
+		if (node.left == null && node.right == null) {
+			this.removeFromParent(node);
+		} else if (node.left != null && node.right == null) {
+			node.value = node.left.value;
+			node.left = node.left.left;
+		} else if (node.right != null && node.left == null) {
+			node.value = node.right.value;
+			node.right =  node.right.right;
+		} else {
+			ContactNode replacement = leftMostChild(node.right);			
+			remove(replacement);
+			node.value = replacement.value;
+		}
+	}
+
+	private void removeFromParent(ContactNode node) {
+		ContactNode nextNode = firstNode;
+		while (true) {
+			if (node.value.name.compareTo(nextNode.value.name) < 0) {
+				if (nextNode.left != null) {
+					if (node.value.name.compareTo(nextNode.left.value.name) == 0) {
+						nextNode.left = null;
+						return;
+					}
+					nextNode = nextNode.left;
+				}
+			} else if (node.value.name.compareTo(nextNode.value.name) > 0) {
+				if (nextNode.right != null) {
+					if (node.value.name.compareTo(nextNode.right.value.name) == 0) {
+						nextNode.right = null;
+						return;
+					}
+					nextNode = nextNode.right;
+				}
+			}
+		}
+	}
+
+	private ContactNode leftMostChild(ContactNode node) {
+		while (node.left != null) {
+			node = node.left;
+		}
+		return node;
 	}
 
 }
--- src/week3/trees/phonebook/Test.java
+++ src/week3/trees/phonebook/Test.java
@@ -4,17 +4,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		PhoneBook book = new PhoneBook();
-		book.insert(new Contact("Didi", 123));
-		book.insert(new Contact("Didito", 1263));
-		book.insert(new Contact("Gosho", 13));
-		//book.lookup("Didi");
-		//book.lookup("Gosho");
-		book.insert(new Contact("Gosho", 134));
-		book.insert(new Contact("Ani", 134));
-		book.insert(new Contact("Misho", 134));
-		book.insert(new Contact("Bobi", 134));
-		//book.lookup("Gosho");
-		//book.lookup("Bla");
+		book.insert(new Contact("Gosho", 5));
+		book.insert(new Contact("Didi", 3));
+		book.insert(new Contact("Kiko", 7));
+		book.insert(new Contact("Ani", 1));
+		book.insert(new Contact("Emo", 4));
+		book.insert(new Contact("Ivan", 6));
+		book.insert(new Contact("Misho", 8));
+		book.insert(new Contact("Bobi", 2));
+		
+		book.remove("Bobi");
+		
 		book.list();
 	}
 
