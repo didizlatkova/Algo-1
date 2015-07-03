a034fcad47b15f38be2d595836de6e2872e60e28
Didi <didi.zlatkova93@gmail.com>
2015-07-03 20:47:00 +0300

    made PhoneBook testable by maycamp

--- src/week2/searching/phonebook/PhoneBook.java
+++ src/week2/searching/phonebook/PhoneBook.java
@@ -1,21 +1,46 @@
 package week2.searching.phonebook;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class PhoneBook {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] input = sc.nextLine().split(" ");
+		int bookLength = Integer.parseInt(input[0]);
+		int queryLength = Integer.parseInt(input[1]);
+		Contact[] phoneBook = new Contact[bookLength];
+		for (int i = 0; i < bookLength; i++) {
+			String[] contact = sc.nextLine().split(" ");
+			phoneBook[i] = new Contact(contact[1], Integer.parseInt(contact[0]));
+		}
+
+		int[] numbers = new int[queryLength];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = Integer.parseInt(sc.nextLine());
+		}
+
+		String[] names = lookupNames(phoneBook, numbers);
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+
+		sc.close();
+	}
 
 	public static String[] lookupNames(Contact[] book, int[] numbers) {
 		Contact[] phoneBook = book.clone();
 		Arrays.sort(phoneBook);
 		String[] names = new String[numbers.length];
-		
+
 		for (int i = 0; i < numbers.length; i++) {
 			names[i] = binarySearch(phoneBook, numbers[i]);
 		}
-		
+
 		return names;
 	}
-	
+
 	private static String binarySearch(Contact[] array, int number) {
 		int left = 0;
 		int right = array.length - 1;
@@ -36,4 +61,24 @@ public class PhoneBook {
 		return "not found";
 	}
 
+	private static class Contact implements Comparable<Contact> {
+
+		public String name;
+		public int phone;
+
+		public Contact(String name, int phone) {
+			this.name = name;
+			this.phone = phone;
+		}
+
+		@Override
+		public int compareTo(Contact o) {
+			if (this.phone == o.phone) {
+				return 0;
+			}
+
+			return this.phone > o.phone ? 1 : -1;
+		}
+
+	}
 }
