03b55f5c305c4098d1288a2482d25bcba77b67e6
Didi <didi.zlatkova93@gmail.com>
2015-07-04 11:22:27 +0300

    made Median testable by maycamp

--- src/week3/trees/median/Median.java
+++ src/week3/trees/median/Median.java
@@ -1,8 +1,20 @@
 package week3.trees.median;
 
-import week2.binarytrees.heapsort.*;
+import java.util.Scanner;
 
 public class Median {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.nextLine());
+		String[] input = sc.nextLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			System.out.println(insert(Integer.parseInt(input[i])));
+		}
+
+		sc.close();
+	}
 
 	private static MinHeap minHeap = new MinHeap(50);
 	private static MaxHeap maxHeap = new MaxHeap(50);
@@ -37,4 +49,181 @@ public class Median {
 
 		return median;
 	}
+
+	private static class MaxHeap {
+
+		private int[] array;
+		private int accommodated = 0;
+
+		public MaxHeap(int capacity) {
+			array = new int[capacity + 1];
+		}
+
+		public int getMax() {
+			if (accommodated == 0) {
+				throw new IndexOutOfBoundsException();
+			}
+
+			return array[1];
+		}
+
+		public void insert(int element) {
+			if (accommodated == array.length) {
+				throw new IndexOutOfBoundsException();
+			}
+
+			accommodated++;
+			int index = accommodated;
+			array[index] = element;
+
+			while (index / 2 > 0) {
+				if (array[index] > array[index / 2]) {
+					switchElements(index, index / 2);
+					index /= 2;
+				} else {
+					break;
+				}
+			}
+		}
+
+		private void switchElements(int i, int j) {
+			int save = array[i];
+			array[i] = array[j];
+			array[j] = save;
+		}
+
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("[");
+			for (int i = 1; i < array.length; i++) {
+				sb.append(array[i] + ", ");
+			}
+			sb.append("]");
+
+			return sb.toString();
+		}
+
+		public void removeMax() {
+			array[1] = array[accommodated];
+			array[accommodated] = 0;
+			accommodated--;
+
+			int index = 1;
+			while (index * 2 <= accommodated) {
+				if (index * 2 + 1 <= accommodated) {
+					if (array[index * 2] < array[index * 2 + 1]
+							&& array[index] < array[index * 2 + 1]) {
+						switchElements(index, index * 2 + 1);
+						index = index * 2 + 1;
+						continue;
+					}
+				}
+
+				if (array[index] < array[index * 2]) {
+					switchElements(index, index * 2);
+					index *= 2;
+				} else {
+					break;
+				}
+			}
+		}
+
+		public boolean isEmpty() {
+			return accommodated == 0;
+		}
+
+		public int size() {
+			return accommodated;
+		}
+
+	}
+
+	private static class MinHeap {
+
+		private int[] array;
+		private int accommodated = 0;
+
+		public MinHeap(int capacity) {
+			array = new int[capacity + 1];
+		}
+
+		public int getMin() {
+			if (accommodated == 0) {
+				throw new IndexOutOfBoundsException();
+			}
+
+			return array[1];
+		}
+
+		public void insert(int element) {
+			if (accommodated == array.length) {
+				throw new IndexOutOfBoundsException();
+			}
+
+			accommodated++;
+			int index = accommodated;
+			array[index] = element;
+
+			while (index / 2 > 0) {
+				if (array[index] < array[index / 2]) {
+					switchElements(index, index / 2);
+					index /= 2;
+				} else {
+					break;
+				}
+			}
+		}
+
+		private void switchElements(int i, int j) {
+			int save = array[i];
+			array[i] = array[j];
+			array[j] = save;
+		}
+
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("[");
+			for (int i = 1; i < array.length; i++) {
+				sb.append(array[i] + ", ");
+			}
+			sb.append("]");
+
+			return sb.toString();
+		}
+
+		public void removeMin() {
+			array[1] = array[accommodated];
+			array[accommodated] = 0;
+			accommodated--;
+
+			int index = 1;
+			while (index * 2 <= accommodated) {
+				if (index * 2 + 1 <= accommodated) {
+					if (array[index * 2] > array[index * 2 + 1]
+							&& array[index] > array[index * 2 + 1]) {
+						switchElements(index, index * 2 + 1);
+						index = index * 2 + 1;
+						continue;
+					}
+				}
+
+				if (array[index] > array[index * 2]) {
+					switchElements(index, index * 2);
+					index *= 2;
+				} else {
+					break;
+				}
+			}
+		}
+
+		public boolean isEmpty() {
+			return accommodated == 0;
+		}
+
+		public int size() {
+			return accommodated;
+		}
+
+	}
+
 }
