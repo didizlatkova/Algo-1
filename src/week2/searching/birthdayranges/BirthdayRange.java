d6d0eafcacb1a49217cc3693ed101416d962d5c8
Didi <didi.zlatkova93@gmail.com>
2015-07-07 23:30:06 +0300

    changes to Birthday Range

--- src/week2/searching/birthdayranges/BirthdayRange.java
+++ src/week2/searching/birthdayranges/BirthdayRange.java
@@ -1,30 +1,34 @@
 package week2.searching.birthdayranges;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BirthdayRange {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 
-		String[] input = sc.nextLine().split(" ");
+		String[] input = reader.readLine().split(" ");
 		int numberOfPeople = Integer.parseInt(input[0]);
 		int numberOfRanges = Integer.parseInt(input[1]);
-		String[] birthdaysInput = sc.nextLine().split(" ");
+		String[] birthdaysInput = reader.readLine().split(" ");
 		int[] birthdays = new int[numberOfPeople];
 		for (int i = 0; i < numberOfPeople; i++) {
 			birthdays[i] = Integer.parseInt(birthdaysInput[i]);
 		}
 
 		int[][] ranges = new int[numberOfRanges][2];
+		String[] range = null;
 		for (int i = 0; i < numberOfRanges; i++) {
-			String[] range = sc.nextLine().split(" ");
+			range = reader.readLine().split(" ");
 			ranges[i][0] = Integer.parseInt(range[0]);
 			ranges[i][1] = Integer.parseInt(range[1]);
 		}
 
+		reader.close();
 		birthdaysCount(birthdays, ranges);
-
 	}
 
 	public static void birthdaysCount(int[] birthdays, int[][] ranges) {
