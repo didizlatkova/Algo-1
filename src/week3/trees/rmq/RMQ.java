5ae7c4efe9950a5ff4ae0ed9d1982d1e2a9ff683
didizlatkova <didi.zlatkova93@gmail.com>
2015-07-08 16:47:57 +0300

    changed RMQ

--- src/week3/trees/rmq/RMQ.java
+++ src/week3/trees/rmq/RMQ.java
@@ -1,52 +1,42 @@
 package week3.trees.rmq;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RMQ {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String firstLine = sc.nextLine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 
-		String[] array = firstLine.split(" ");
-		if (array.length != 2) {
-			sc.close();
-			throw new IllegalArgumentException("Invalid command");
-		}
-
-		int vectorLength = 0;
-		int numberOfCommands = 0;
-
-		try {
-			vectorLength = Integer.parseInt(array[0]);
-			numberOfCommands = Integer.parseInt(array[1]);
-		} catch (NumberFormatException e) {
-			sc.close();
-			throw new IllegalArgumentException("Invalid command");
-		}
+		String[] array = reader.readLine().split(" ");
+		int vectorLength = Integer.parseInt(array[0]);
+		int numberOfCommands = Integer.parseInt(array[1]);
 
 		int[] vector = new int[vectorLength];
-		String vectorNumbers = sc.nextLine();
-		String[] numbersArray = vectorNumbers.split(" ");
+		String[] numbersArray = reader.readLine().split(" ");
 
-		try {
-			for (int i = 0; i < numbersArray.length; i++) {
-				vector[i] = Integer.parseInt(numbersArray[i]);
-			}
-		} catch (NumberFormatException e) {
-			sc.close();
-			throw new IllegalArgumentException("Invalid command");
+		for (int i = 0; i < numbersArray.length; i++) {
+			vector[i] = Integer.parseInt(numbersArray[i]);
 		}
 
-		CommandParser parser = new CommandParser(vector);
+		RMQ rmq = new RMQ(vector);
 
 		for (int i = 0; i < numberOfCommands; i++) {
-			parser.parse(sc.nextLine());
-		}
+			String[] commandParts = reader.readLine().split(" ");
+			String command = commandParts[0];
+			int firstParam = Integer.parseInt(commandParts[1]);
+			int secondParam = Integer.parseInt(commandParts[2]);
 
-		sc.close();
+			if (command.equals("set")) {
+				rmq.set(firstParam, secondParam);
+			} else if (command.equals("min")) {
+				System.out.println(rmq.min(firstParam, secondParam));
+			}
+		}
 	}
-	
+
 	private int[] array;
 	private final int originalLength;
 
@@ -61,6 +51,10 @@ public class RMQ {
 
 		for (int i = 0; i < input.length; i++) {
 			array[originalLength + i] = input[i];
+		}
+
+		for (int i = input.length; i < originalLength - input.length; i++) {
+			array[originalLength + i] = Integer.MAX_VALUE;
 		}
 
 		for (int i = originalLength - 1; i > 0; i--) {
@@ -86,7 +80,7 @@ public class RMQ {
 		endIndex += originalLength;
 
 		while (startIndex < endIndex) {
-			if (startIndex % 2 == 1) {
+			if ((startIndex) % 2 == 1) {
 				// right child
 				min = Math.min(min, array[startIndex]);
 				startIndex++;
@@ -110,77 +104,6 @@ public class RMQ {
 		}
 
 		return min;
-	}
-
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("[");
-		for (int i = 1; i < array.length; i++) {
-			sb.append(array[i] + ", ");
-		}
-		sb.append("]");
-
-		return sb.toString();
-	}
-
-	private static class CommandParser {
-
-		private RMQ rmq;
-
-		public CommandParser(int[] array) {
-			rmq = new RMQ(array);
-		}
-
-		public void parse(String command) {
-			if (command.startsWith("set")) {
-				set(command);
-			} else if (command.startsWith("min")) {
-				min(command);
-			} else {
-				throw new IllegalArgumentException("Invalid command");
-			}
-		}
-
-		private void set(String command) {
-			String[] contactArray = command.split(" ");
-			if (contactArray.length != 3) {
-				throw new IllegalArgumentException("Invalid command");
-			}
-
-			int index = 0;
-			int value = 0;
-			try {
-				index = Integer.parseInt(contactArray[1]);
-				value = Integer.parseInt(contactArray[2]);
-			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Invalid command");
-			}
-
-			if (index < 0) {
-				throw new IllegalArgumentException("Invalid command");
-			}
-
-			rmq.set(index, value);
-		}
-
-		private void min(String command) {
-			String[] contactArray = command.split(" ");
-			if (contactArray.length != 3) {
-				throw new IllegalArgumentException("Invalid command");
-			}
-
-			int startIndex = 0;
-			int endIndex = 0;
-			try {
-				startIndex = Integer.parseInt(contactArray[1]);
-				endIndex = Integer.parseInt(contactArray[2]);
-			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Invalid command");
-			}
-
-			System.out.println(rmq.min(startIndex, endIndex));
-		}
-
 	}
 
 }
