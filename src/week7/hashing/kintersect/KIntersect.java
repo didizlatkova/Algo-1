0a445846db5dcc98ccaa04123dc8850701e4002c
Didi <didi.zlatkova93@gmail.com>
2015-07-17 00:17:12 +0300

    k intersect

---
+++ src/week7/hashing/kintersect/KIntersect.java
@@ -0,0 +1,17 @@
+package week7.hashing.kintersect;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KIntersect {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		int n = Integer.parseInt(reader.readLine());
+		
+		
+	}
+
+}
